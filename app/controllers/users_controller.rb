c44db20480e21ca9dd35acef58a88625a1237b6e
dianawhalen <whalendianaj@gmail.com>
2021-05-23 21:55:35 -0600

    add user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
