4659a6d20b8028ce5ff96b35c59e8a598bcc2224
dianawhalen <whalendianaj@gmail.com>
2021-05-26 03:28:05 -0600

    add submissions controller actions + views

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,3 +1,22 @@
 class SubmissionsController < ApplicationController
   before_action :redirect_if_not_signed_in
+
+  def new
+    @submission = Submission.new
+  end
+
+  def create
+    @submission = current_user.submissions.build(submission_params)
+    if @submission.save
+      redirect_to submissions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:title, :url, :notes)
+  end
 end
