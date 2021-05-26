30d227a46aad05ff77ff34a3858321f9f7a8709f
dianawhalen <whalendianaj@gmail.com>
2021-05-25 19:40:15 -0600

    add signin route, action, and form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "We're sorry, some of the information you entered was incorrect, please try again."
+      redirect_to signin_path
+    end
   end
 
   def destroy
