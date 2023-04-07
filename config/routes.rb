56f6372408ee0a8a113d0587830c2b6f7666623f
dianawhalen <whalendianaj@gmail.com>
2023-04-06 21:35:44 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,29 @@
 Rails.application.routes.draw do
-  get 'collections/index'
-  get 'collections/show'
-  get 'collections/new'
-  get 'collections/edit'
-  get 'collections/create'
-  get 'collections/update'
-  get 'collections/destroy'
-  get 'upvotes/index'
-  get 'upvotes/show'
-  get 'upvotes/new'
-  get 'upvotes/edit'
-  get 'upvotes/create'
-  get 'upvotes/update'
-  get 'upvotes/destroy'
-  get 'submissions/index'
-  get 'submissions/show'
-  get 'submissions/new'
-  get 'submissions/edit'
-  get 'submissions/create'
-  get 'submissions/update'
-  get 'submissions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
-  get 'sessions/home'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :collections
-  resources :upvotes
-  resources :submissions
-  resources :users
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+
+  delete '/signout', to: 'sessions#destroy'
+
+  root 'sessions#home'
+
+  resource :sessions, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :submissions, only: [:new, :create, :edit, :update, :destroy]
+    resources :collections, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+
+  resources :submissions, only: [:index, :show] do
+    resources :upvotes, only: [:create, :destroy]
+  end
+
+  resources :collections, only: [:show] do
+    resources :collection_submissions, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
