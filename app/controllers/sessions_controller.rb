1ac16cf6c6dd44f3e24816cebba621e9209a83c7
dianawhalen <whalendianaj@gmail.com>
2023-05-19 17:36:27 -0600

    refactored buttons and consolidated styles

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,12 +12,12 @@ class SessionsController < ApplicationController
       @user = User.from_omniauth(auth)
       @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to root_path
     else
       @user = User.find_by(email: params[:user][:email])
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        redirect_to user_path(@user), notice: "Logged in successfully!"
+        redirect_to root_path, notice: "Logged in successfully!"
       else
         redirect_to signin_path, alert: "We're sorry, some of the information you entered was incorrect, please try again."
       end
