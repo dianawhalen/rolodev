b124811149090eb2330f42916a5e3a8fb838552c
dianawhalen <whalendianaj@gmail.com>
2023-06-07 14:05:48 -0600

    reset and seeded database

--- db/schema.rb
+++ db/schema.rb
@@ -2,21 +2,20 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_13_003738) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_05_13_003738) do
   create_table "collection_submissions", force: :cascade do |t|
     t.integer "collection_id", null: false
     t.integer "submission_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["collection_id"], name: "index_collection_submissions_on_collection_id"
     t.index ["submission_id"], name: "index_collection_submissions_on_submission_id"
   end
@@ -24,8 +23,8 @@ ActiveRecord::Schema.define(version: 2023_05_13_003738) do
   create_table "collections", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_collections_on_user_id"
   end
 
@@ -34,16 +33,16 @@ ActiveRecord::Schema.define(version: 2023_05_13_003738) do
     t.string "url"
     t.text "notes"
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
   create_table "upvoted_submissions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "submission_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["submission_id"], name: "index_upvoted_submissions_on_submission_id"
     t.index ["user_id"], name: "index_upvoted_submissions_on_user_id"
   end
@@ -51,8 +50,8 @@ ActiveRecord::Schema.define(version: 2023_05_13_003738) do
   create_table "upvotes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "submission_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["submission_id"], name: "index_upvotes_on_submission_id"
     t.index ["user_id"], name: "index_upvotes_on_user_id"
   end
@@ -61,8 +60,8 @@ ActiveRecord::Schema.define(version: 2023_05_13_003738) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "from_omniauth"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,63 +6,65 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# submissions_data = [
-#   { title: "First Post", url: "http://example.com/1", notes: "This is the first post." },
-#   { title: "Second Post", url: "http://example.com/2", notes: "This is the second post." },
-#   { title: "Third Post", url: "http://example.com/3", notes: "This is the third post." },
-#   { title: "Fourth Post", url: "http://example.com/4", notes: "This is the fourth post." },
-#   { title: "Fifth Post", url: "http://example.com/5", notes: "This is the fifth post." },
-#   { title: "Sixth Post", url: "http://example.com/6", notes: "This is the sixth post." },
-#   { title: "Seventh Post", url: "http://example.com/7", notes: "This is the seventh post." },
-#   { title: "Eighth Post", url: "http://example.com/8", notes: "This is the eighth post." },
-#   { title: "Ninth Post", url: "http://example.com/9", notes: "This is the ninth post." },
-#   { title: "Tenth Post", url: "http://example.com/10", notes: "This is the tenth post." },
-#   { title: "Eleventh Post", url: "http://example.com/11", notes: "This is the eleventh post." },
-#   { title: "Twelfth Post", url: "http://example.com/12", notes: "This is the twelfth post." },
-#   { title: "Thirteenth Post", url: "http://example.com/13", notes: "This is the thirteenth post." },
-#   { title: "Fourteenth Post", url: "http://example.com/14", notes: "This is the fourteenth post." },
-#   { title: "Fifteenth Post", url: "http://example.com/15", notes: "This is the fifteenth post." },
-#   { title: "Sixteenth Post", url: "http://example.com/16", notes: "This is the sixteenth post." },
-#   { title: "Seventeenth Post", url: "http://example.com/17", notes: "This is the seventeenth post." },
-#   { title: "Eighteenth Post", url: "http://example.com/18", notes: "This is the eighteenth post." },
-#   { title: "Nineteenth Post", url: "http://example.com/19", notes: "This is the nineteenth post." },
-#   { title: "Twentieth Post", url: "http://example.com/20", notes: "This is the twentieth post." },
-# ]
-#
-# collection_names = ["First Collection", "Second Collection", "Third Collection", "Fourth Collection", "Fifth Collection", "Sixth Collection", "Seventh Collection", "Eighth Collection", "Ninth Collection", "Tenth Collection"]
-#
-#
-# 5.times do
-#   user = User.create!(
-#     username: Faker::Internet.username,
-#     email: Faker::Internet.email,
-#     password: 'temppassword',
-#     from_omniauth: false
-#   )
-#
-#   user.update!(password: "#{user.username}#{'%03d' % user.id}")
-#
-#   2.times do
-#     user.collections.create!(
-#       name: collection_names.shift
-#     )
-#   end
-#
-#   4.times do
-#     user.submissions.create!(submissions_data.shift)
-#   end
-# end
-#
-# all_collections = Collection.all
-# all_submissions = Submission.all
-#
-# all_collections.each do |collection|
-#   collection.collection_submissions.create!(submission_id: all_submissions.sample.id)
-# end
-#
-# all_submissions.each do |submission|
-#   collection = all_collections.sample
-#   unless collection.submissions.include?(submission)
-#     collection.collection_submissions.create!(submission_id: submission.id)
-#   end
-# end
+
+
+submissions_data = [
+  { title: "Venturefy.ai", url: "https://www.venturefy.com/", notes: "Explore verified corporate relationships for any company. ✅" },
+  { title: "Movie Deep Search by AI Keytalk", url: "https://www.mycelebs.com/deepsearch", notes: "Recommendation AI that knows every movie ever created." },
+  { title: "Zipy", url: "https://www.zipy.ai/", notes: "Debug instantly with Session Playback and Monitoring in one." },
+  { title: "RADAAR", url: "https://www.radaar.io/", notes: "Not just another scheduling tool!" },
+  { title: "iStory", url: "https://studio.istorylive.com/", notes: "The power of voice activated content is just an iStory away." },
+  { title: "Spinach.io", url: "https://spinach.io/", notes: "Crush your daily standup." },
+  { title: "Zeda.io", url: "https://zeda.io/", notes: "AI-powered Product Discovery For Customer-focused Teams." },
+  { title: "Searchie", url: "https://searchie.io/", notes: "Create, Manage, & Sell Your Content All In One Place." },
+  { title: "Seodity", url: "https://seodity.com/", notes: "All-in-one SEO tool with AI." },
+  { title: "MakerBox", url: "https://www.makerbox.club/", notes: "Marketing resources for Solopreneurs." },
+  { title: "HireQuotient", url: "https://hirequotient.com/", notes: "A Great Hiring Story Begins with HireQuotient." },
+  { title: "Phyllo", url: "https://www.getphyllo.com/", notes: "The Universal API for Creator Data" },
+  { title: "Cascadeur", url: "https://cascadeur.com/", notes: "AI-assisted keyframe animation software." },
+  { title: "SoWork", url: "https://www.sowork.com/", notes: "Drive faster, better, more connected remote work." },
+  { title: "SingleStore Kai for MongoDB", url: "https://www.singlestore.com/", notes: "100x-1000x faster analytics for MongoDB applications." },
+  { title: "Taskade", url: "https://www.taskade.com/", notes: "Build a second brain for your team. Chat, organize, and get work done in one unified workspace." },
+  { title: "Codesphere", url: "https://codesphere.com/", notes: "Deploy in less than 5s." },
+  { title: "vidBoard", url: "https://www.vidboard.ai/", notes: "Make video presentations with AI. 🚀" },
+  { title: "Jam", url: "https://jam.dev/", notes: "1 click to capture a perfect bug report. 🚀" },
+  { title: "Kairn", url: "https://kairn.app/", notes: "Rock your days." },
+]
+
+collection_names = ["Dev Tools", "Cool Sites", "Useful Resources", "Productivty", "Utilities", "Learning", "Networking", "Backend Tools", "Marketing", "SEO Templates"]
+
+
+5.times do
+  user = User.create!(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: 'temppassword',
+    from_omniauth: false
+  )
+
+  user.update!(password: "#{user.username}#{'%03d' % user.id}")
+
+  2.times do
+    user.collections.create!(
+      name: collection_names.shift
+    )
+  end
+
+  4.times do
+    user.submissions.create!(submissions_data.shift)
+  end
+end
+
+all_collections = Collection.all
+all_submissions = Submission.all
+
+all_collections.each do |collection|
+  collection.collection_submissions.create!(submission_id: all_submissions.sample.id)
+end
+
+all_submissions.each do |submission|
+  collection = all_collections.sample
+  unless collection.submissions.include?(submission)
+    collection.collection_submissions.create!(submission_id: submission.id)
+  end
+end
